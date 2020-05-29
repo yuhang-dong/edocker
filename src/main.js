be851b368d4b9c0bd746e19827126507fe6c3dbb
dongyuhang <dongyuhang@gmail.com>
2020-05-29 14:21:31 +0800

    import vuerouter

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.js';
 import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
-
+import VueRouter from 'vue-router'
+import router from './router'
 Vue.config.productionTip = false
 
 Vue.use(Antd);
+Vue.use(VueRouter);
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
