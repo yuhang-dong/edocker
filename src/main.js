da5204b56006069aa46a765cff24bbafa7bc7fc6
dongyuhang <dongyuhang@gmail.com>
2020-06-22 14:01:23 +0800

    update json-viewer usage

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'ant-design-vue/dist/antd.css';
 import VueRouter from 'vue-router'
 import router from './router'
 import store from './store'
+import JsonViewer from 'vue-json-viewer'
 Vue.config.productionTip = false
 
 // import http
@@ -14,7 +15,7 @@ Vue.prototype.$DOCKER = dockerode;
 // import other
 Vue.use(Antd);
 Vue.use(VueRouter);
-
+Vue.use(JsonViewer);
 new Vue({
   render: h => h(App),
   router,
